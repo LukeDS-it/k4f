61150197eeb474dc87a4725f47caff5df3652cc4
LukeDS-it <luca.mcgyver@gmail.com>
2020-02-22 14:07:58 +0100

    using typed library

--- src/main/kotlin/Index.kt
+++ src/main/kotlin/Index.kt
@@ -1,8 +1,10 @@
+import it.ldsoftware.wrappers.firebase.FunctionBuilder
+
 external fun require(module: String): dynamic
 external val exports: dynamic
 
 fun main() {
-    val functions = require("firebase-functions")
+    val functions = require("firebase-functions").uncheckedCast<FunctionBuilder>()
 
     exports.helloWorld = functions.https.onRequest { request, response ->
         console.log("Request headers: ${request.headers}")
